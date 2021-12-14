4c3ff05fde55f81b8547f752eb7fc7f690869e9c
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2021-12-14 22:47:35 +0100

    Added todos, and fields for block class.

---
+++ src/blockchain/Block.java
@@ -0,0 +1,15 @@
+package blockchain;
+import java.util.ArrayList;
+
+public class Block {
+	private String prevHash;
+	private ArrayList<Transaction> transactions;
+	private String nonce;
+	private String hash;
+	
+	public Block() {
+		// TODO - implement BlockHeader class
+		// Generally, go through https://www.oreilly.com/library/view/mastering-bitcoin/9781491902639/ch01.html
+		
+	}
+}
