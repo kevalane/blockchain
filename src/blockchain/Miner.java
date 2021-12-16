44f4679cdef384703bc2472aed29ca3c01584527
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2021-12-16 10:39:44 +0100

    implemented mining class. gotta test.

--- src/blockchain/Miner.java
+++ src/blockchain/Miner.java
@@ -1,22 +1,28 @@
 package blockchain;
 
+import java.security.NoSuchAlgorithmException;
+
 public class Miner {
-	private String nonce;
+	private int nonce;
 	private String header;
 	private int difficulty;
 	private String prefix;
 	
-	public Miner (String header, int difficulty) {
+	public Miner (String header, int difficulty) throws NoSuchAlgorithmException {
 		this.header = header;
 		this.difficulty = difficulty;
-		this.prefix = 
-		this.mine();
+		this.prefix = this.fixPrefix();
+		System.out.println(this.mine());
 	}
 	
-	private boolean mine () {
-		Hash hash;
-//		while ()
-		return true;
+	private String mine () throws NoSuchAlgorithmException {
+		this.nonce = 0;
+		Hash hash = new Hash(this.header + String.valueOf(this.nonce));
+		while (!hash.getHash().startsWith(this.prefix)) {
+			this.nonce++;
+			hash = new Hash(this.header + String.valueOf(this.nonce));
+		}
+		return hash.getHash();
 	}
 		
 	// Fix prefix
