99f9d99e1eb6fd2aaf5a237cf95e74b18f5eb94b
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2021-12-16 10:14:19 +0100

    Tested hash in main class.

--- src/blockchain/Main.java
+++ src/blockchain/Main.java
@@ -5,7 +5,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		try {
+			Hash hash = new Hash("Hello World");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
