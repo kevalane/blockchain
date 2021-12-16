2c4047a7e0286d6eb99d86875e2af74dd393ca95
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2021-12-16 10:44:21 +0100

    Tested miner, works with different difficulties.

--- src/blockchain/Main.java
+++ src/blockchain/Main.java
@@ -4,11 +4,8 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		try {
-			for (int i = 0; i < 100; i++) {
-				Hash hash = new Hash(String.valueOf(i));
-			}
+			Miner miner = new Miner("thisIsAHeader", 5);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
