7252f79a65553a9d24fef75af4ee5f9b7bd048c4
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-01-04 00:38:49 +0100

    Verification not working, needs work.

--- src/blockchain/Main.java
+++ src/blockchain/Main.java
@@ -12,30 +12,36 @@ public class Main {
 //		}
 		
 		// Lets generate some transactions
-		Transaction t1 = new Transaction(1, "Bob", "Alice", "feaaef", 10);
-		Transaction t2 = new Transaction(2, "Alice", "Charlie", "feaaefef", 5);
-		Transaction t3 = new Transaction(3, "Charlie", "Bob", "afeaefaefaef", 20);
-		Transaction t4 = new Transaction(4, "Alice", "Bob", "grsagrf", 50);
+		// Transaction t1 = new Transaction(1, "Bob", "Alice", "feaaef", 10);
+		// Transaction t2 = new Transaction(2, "Alice", "Charlie", "feaaefef", 5);
+		// Transaction t3 = new Transaction(3, "Charlie", "Bob", "afeaefaefaef", 20);
+		// Transaction t4 = new Transaction(4, "Alice", "Bob", "grsagrf", 50);
 		
-		Hash genesisHash = new Hash("genesisblock");
-		Block genesisBlock = new Block(genesisHash.getHash());
-		genesisBlock.addTransaction(t1);
-		genesisBlock.addTransaction(t2);
-		genesisBlock.addTransaction(t3);
-//		genesisBlock.addTransaction(t4);
-		System.out.println(genesisBlock.getRawData());
-		Miner miner = new Miner(genesisBlock.getRawData(), 5);
+		// Hash genesisHash = new Hash("genesisblock");
+		// Block genesisBlock = new Block(genesisHash.getHash());
+		// genesisBlock.addTransaction(t1);
+		// genesisBlock.addTransaction(t2);
+		// genesisBlock.addTransaction(t3);
+		// System.out.println(genesisBlock.getRawData());
+		// Miner miner = new Miner(genesisBlock.getRawData(), 4);
 		
-		// Update genesis block
-		genesisBlock.setHash(miner.getProofOfWorkHash());
-		genesisBlock.setNonce(miner.getNonce());
+		// genesisBlock.setHash(miner.getProofOfWorkHash());
+		// genesisBlock.setNonce(miner.getNonce());
 		
-		Block genesisBlockPlus1 = new Block(genesisBlock.getHash());
-		genesisBlockPlus1.addTransaction(t1);
-		genesisBlockPlus1.addTransaction(t2);
-		genesisBlockPlus1.addTransaction(t4);
-		System.out.println(genesisBlockPlus1.getRawData());
-		Miner miner2 = new Miner(genesisBlockPlus1.getRawData(), 5);
+		// Block genesisBlockPlus1 = new Block(genesisBlock.getHash());
+		// genesisBlockPlus1.addTransaction(t1);
+		// genesisBlockPlus1.addTransaction(t2);
+		// genesisBlockPlus1.addTransaction(t4);
+		// System.out.println(genesisBlockPlus1.getRawData());
+		// Miner miner2 = new Miner(genesisBlockPlus1.getRawData(), 4);
+
+		try {
+			Sign sign = new Sign();
+			System.out.println(sign.signMessage("Hello wold"));
+			sign.verify(sign.signMessage("Hello wold"));
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 
 }
--- src/blockchain/Sign.java
+++ src/blockchain/Sign.java
@@ -8,27 +8,39 @@ import java.security.Signature;
 public class Sign {
 
     KeyPair keyPair;
+    Signature sig;
 
     public Sign() throws Exception {
         this.keyPair = this.getKeyPair();
+        sig = Signature.getInstance("SHA1WithRSA");
     }
 
     public String signMessage(String message) throws Exception {
         byte[] data = message.getBytes("UTF8");
-        Signature sig = Signature.getInstance("SHA1WithRSA");
         sig.initSign(this.keyPair.getPrivate());
         sig.update(data);
+        System.out.println(sig);
         byte[] signatureBytes = sig.sign();
         return toHex(signatureBytes);
     }
 
+    public boolean verify(String signature) throws Exception {
+        byte[] data = signature.getBytes("UTF8");
+        sig.initVerify(keyPair.getPublic());
+        sig.update(data);
+        System.out.println(sig.verify(data));
+        return true;
+        
+    }
+
     private KeyPair getKeyPair() throws NoSuchAlgorithmException {
         KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-        kpg.initialize(4096);
+        kpg.initialize(2048);
         return kpg.genKeyPair();
     }
 
     // Convert sha256 byte array to hex string
+    // TODO: REMOVE and implement one static shared by hash and this class
 	private String toHex(byte[] hash) {
 		BigInteger num = new BigInteger(1, hash);
 		StringBuilder hex = new StringBuilder(num.toString(16));
