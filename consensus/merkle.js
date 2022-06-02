51ce282aefe69ec637eacd731b181011a8a556aa
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-06-02 20:11:55 +0200

    begun iumplementation of leaf formation

--- consensus/merkle.js
+++ consensus/merkle.js
@@ -6,7 +6,27 @@ exports.MerkleTree = void 0;
  */
 class MerkleTree {
     constructor(transactions) {
-        this.merkleRoot = "";
+        this.merkleRootHash = "";
+        this.merkleRootNode = new MerkleTreeNode();
+        this.createMerkleTree(transactions);
+    }
+    createMerkleTree(transactions) {
+        const paddedTransactions = this.padTransactions(transactions);
+    }
+    /**
+     * Helper function to create the base of merkle tree.
+     * Has to be a power of 2 number of leafs in tree.
+     * @param transactions the transactions in the block.
+     */
+    padTransactions(transactions) {
+        let nearest = (1 << 31 - Math.clz32(transactions.length));
+        if (nearest < transactions.length)
+            nearest = nearest * 2; // has to be greater
+        let returnArray = new Array(nearest);
+        // add transactions to the padded array
+        for (let i = 0; i < nearest; i++) {
+        }
+        return returnArray;
     }
 }
 exports.MerkleTree = MerkleTree;
--- consensus/merkle.ts
+++ consensus/merkle.ts
@@ -2,11 +2,33 @@
  * Create Merkle Tree from an array of transactions
  */
 class MerkleTree {
-    private merkleRoot: string;
-    
+    private merkleRootHash: string;
+    private merkleRootNode: MerkleTreeNode;
 
     constructor(transactions: any[]) {
-        this.merkleRoot = "";
+        this.merkleRootHash = "";
+        this.merkleRootNode = new MerkleTreeNode();
+        this.createMerkleTree(transactions);
+    }
+
+    private createMerkleTree(transactions: any[]) {
+        const paddedTransactions: any[] = this.padTransactions(transactions);
+    }
+
+    /**
+     * Helper function to create the base of merkle tree.
+     * Has to be a power of 2 number of leafs in tree.
+     * @param transactions the transactions in the block.
+     */
+    private padTransactions(transactions: any[]): Array<any> {
+        let nearest: number = (1 << 31 - Math.clz32(transactions.length));
+        if (nearest < transactions.length) nearest = nearest*2; // has to be greater
+        let returnArray: Array<any> =  new Array<any>(nearest);
+        // add transactions to the padded array
+        for (let i = 0; i < nearest; i++) {
+            
+        }
+        return returnArray;
     }
 }
 
@@ -21,27 +43,27 @@ class MerkleTreeNode {
         this.merkleHash = "";
     }
 
-    private getHash() {
+    private getHash(): string {
         return this.merkleHash;
     }
 
-    private getLeftNode() {
+    private getLeftNode(): MerkleTreeNode {
         return this.leftNode;
     }
 
-    private getRightNode() {
+    private getRightNode(): MerkleTreeNode {
         return this.rightNode;
     }
 
-    private setHash(hash: string) {
+    private setHash(hash: string): void {
         this.merkleHash = hash;
     }
 
-    private setLeftNode(n: MerkleTreeNode) {
+    private setLeftNode(n: MerkleTreeNode): void {
         this.leftNode = n;
     }
 
-    private setRightNode(n: MerkleTreeNode) {
+    private setRightNode(n: MerkleTreeNode): void {
         this.rightNode = n;
     }
 }
