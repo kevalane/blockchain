78424f19fc4ed14fea8e201013154de7d030ee30
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-06-02 21:51:37 +0200

    half done merkle tree impl

--- consensus/merkle.ts
+++ consensus/merkle.ts
@@ -3,16 +3,17 @@
  */
 class MerkleTree {
     private merkleRootHash: string;
-    private merkleRootNode: MerkleTreeNode;
+    // private merkleRootNode: MerkleTreeNode;
 
     constructor(transactions: any[]) {
         this.merkleRootHash = "";
-        this.merkleRootNode = new MerkleTreeNode();
+        // this.merkleRootNode = new MerkleTreeNode();
         this.createMerkleTree(transactions);
     }
 
     private createMerkleTree(transactions: any[]) {
         const paddedTransactions: any[] = this.padTransactions(transactions);
+        console.log(paddedTransactions);
     }
 
     /**
@@ -21,12 +22,21 @@ class MerkleTree {
      * @param transactions the transactions in the block.
      */
     private padTransactions(transactions: any[]): Array<any> {
-        let nearest: number = (1 << 31 - Math.clz32(transactions.length));
-        if (nearest < transactions.length) nearest = nearest*2; // has to be greater
-        let returnArray: Array<any> =  new Array<any>(nearest);
-        // add transactions to the padded array
-        for (let i = 0; i < nearest; i++) {
-            
+        let nearestUpwards: number = (1 << 31 - Math.clz32(transactions.length));
+        let nearestDownwards: number;
+        if (nearestUpwards < transactions.length){
+            nearestDownwards = nearestUpwards; // eg. this is 32, but we have 44 transactions
+            nearestUpwards = nearestUpwards*2; // has to be greater, so upwards becomes 64, "closing in" the 44 transactions.
+        } else {
+            nearestDownwards = nearestUpwards/2; // in this case up is 64, transaction is 44, so we make down 64/2=32
+        }
+        let returnArray: Array<any> =  new Array<any>(nearestUpwards); // size of biggest, 64 in our examples
+        // add transactions to the padded array, all upp to down not affected
+        for (let i = 0; i < nearestDownwards; i++) {
+            returnArray[i] = transactions[i];
+        }
+        for (let i = nearestDownwards; i < nearestUpwards; i++) {
+
         }
         return returnArray;
     }
---
+++ index.ts
@@ -0,0 +1,3 @@
+import { MerkleTree } from './consensus/merkle';
+
+let mt: MerkleTree = new MerkleTree([1, 2, 3, 4, 5]);
