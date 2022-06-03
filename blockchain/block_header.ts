245d912179a222aae0b9a0e7f14808151ced3016
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-06-03 13:42:08 +0200

    implemented all functions for blockHeader

--- blockchain/block_header.ts
+++ blockchain/block_header.ts
@@ -1,3 +1,5 @@
+import { createHash } from "crypto";
+
 class BlockHeader {
     private version: number;
     private previousBlockHash: string;
@@ -15,7 +17,25 @@ class BlockHeader {
         this.nonce = 0;
     }
 
-    
+    public getBlockHeaderHash(): string {
+        const firstHash: string = createHash('sha256').update(
+            this.version +
+            this.previousBlockHash +
+            this.merkleRootHash + 
+            this.timeStamp +
+            this.difficulty + 
+            this.nonce
+        ).digest('hex');
+        return createHash('sha256').update(firstHash).digest('hex'); // double hashed
+    }
+
+    public setNonce(n: number): void {
+        this.nonce = n;
+    }
+
+    public setTimeStamp(t: number): void {
+        this.timeStamp = t;
+    }
 }
 
 export { BlockHeader }
