2d51057cca75c75e22ad0cd11ad98457482f6ce1
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-06-03 11:50:15 +0200

    added comments to constructor

--- transactions/transaction.ts
+++ transactions/transaction.ts
@@ -7,12 +7,21 @@ class Transaction {
     private inputs: InputTx[];
     private outputCounter: number;
     private outputs: OutputTx[];
+    private lockTime: Date;
 
-    constructor(v: number, inputs: InputTx[], outputs: OutputTx[]) {
+    /**
+     * Creates a new transaction.
+     * @param v the version the transaction adheres to
+     * @param inputs input transactions
+     * @param outputs output transactions
+     * @param lockTime if 0 execute immediately, otherwise don't include until Date.now() > lockTime
+     */
+    constructor(v: number, inputs: InputTx[], outputs: OutputTx[], lockTime: Date) {
         this.version = v;
         this.inputCounter = inputs.length;
         this.inputs = inputs;
         this.outputCounter = outputs.length;
         this.outputs = outputs;
+        this.lockTime = lockTime;
     }
 }
