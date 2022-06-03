811ab0e9068063aab255cfc9e833d502086697ec
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-06-03 12:51:28 +0200

    added verification

--- transactions/input_tx.ts
+++ transactions/input_tx.ts
@@ -1,4 +1,4 @@
-import { createSign } from "crypto";
+import { createHash, createSign, createVerify } from "crypto";
 import { OutputTx } from "./output_tx";
 import { Transaction } from "./transaction";
 
@@ -6,17 +6,18 @@ class InputTx {
     private transactionHash: string;
     private utxoIndex: number;
     private receiverPublicKey: string;
-    private senderSignature: Buffer | null;
+    private senderSignature: Buffer;
 
     constructor(t_hash: string, utxoIndex: number, pubKey: string) {
         this.transactionHash = t_hash;
         this.utxoIndex = utxoIndex;
         this.receiverPublicKey = pubKey;
-        this.senderSignature = null;
+        this.senderSignature = new Buffer("");
     }
 
     public signInputTx(privateKey: string): void {
-        const concatenatedHash = this.transactionHash + this.receiverPublicKey;
+        const concatenatedHash: string = createHash('sha-256').
+            update(this.transactionHash + this.receiverPublicKey).digest('hex');
         const sign = createSign('SHA256');
         sign.update(concatenatedHash).end();
         const signature = sign.sign(privateKey);
@@ -27,8 +28,10 @@ class InputTx {
     public verify(transaction: Transaction): boolean {
         const utxo: OutputTx | null = transaction.getUtxoByIndex(this.utxoIndex);
         if (utxo == null) return false;
-        
-        
+        const utxoPublicKey: string = utxo.getAddress();
+        const verifier = createVerify('SHA256');
+        verifier.update(createHash('sha-256').update(this.transactionHash + this.receiverPublicKey).digest('hex'));
+        return verifier.verify(utxoPublicKey, this.senderSignature);
     }
 }
 
