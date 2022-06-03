8e692a4a6d9aed888fd9e3d18a2e108f23669e00
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-06-03 12:19:48 +0200

    pausing input implementation to create wallet

--- transactions/input_tx.ts
+++ transactions/input_tx.ts
@@ -1,5 +1,19 @@
 class InputTx {
+    private transactionHash: string;
+    private utxoIndex: number;
+    private receiverPublicKey: string;
+    private senderSignature: string;
 
+    constructor(t_hash: string, utxoIndex: number, pubKey: string) {
+        this.transactionHash = t_hash;
+        this.utxoIndex = utxoIndex;
+        this.receiverPublicKey = pubKey;
+        this.senderSignature = "";
+    }
+
+    public signInputTx(): void {
+        
+    }
 }
 
 export { InputTx }
